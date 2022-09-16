c0b777f0c40437321b11b4ddfe1dc0a17ae24991
Athila da Silva Coelho <athila.coelho@cegas.com.br>
2022-09-16 16:41:34 -0300

    Add project files.

---
+++ VOD.UI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using VOD.UI.Models;
+using Microsoft.AspNetCore.Identity;
+using VOD.Common.Entities;
+
+namespace VOD.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+        private SignInManager<VODUser> _signInManager;
+
+        /*public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }*/
+
+        public HomeController(SignInManager<VODUser> signInMgr)
+        {
+            _signInManager = signInMgr;
+        }
+
+        public IActionResult Index()
+        {
+            if (!_signInManager.IsSignedIn(User))
+                return RedirectToPage("/Account/Login", new { Area = "Identity" });
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+         
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
